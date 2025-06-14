aaf225620eb3c067254a94c6a340d5f79d8abfcd
koalazub <>
2025-06-14 22:46:46 +1000

    feat: upgrade build step in nix, change NS alert to toggle differently

--- flake.nix
+++ flake.nix
@@ -5,21 +5,30 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { nixpkgs, ... }:
     let
-      supportedSystems = [ "x86_64-darwin" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in
     {
-      packages = forAllSystems (system:
-        let 
+      packages = forAllSystems (
+        system:
+        let
           pkgs = nixpkgsFor.${system};
-        in {
+        in
+        {
           default = pkgs.stdenvNoCC.mkDerivation {
             pname = "WhoseDefaultBrowser";
             version = "1.0.0";
-            src = ./.;
+            src = builtins.path {
+              path = ./.;
+              name = "source";
+            };
             buildInputs = [ pkgs.darwin.apple_sdk.frameworks.CoreServices ];
             buildPhase = ''
               export PATH=$PATH:/usr/bin:/usr/local/bin
--- main.swift
+++ main.swift
@@ -1,69 +1,56 @@
 import Cocoa
 
 func appName(fromBundleId appBundleId: String) -> String {
-    return appBundleId.components(separatedBy: ".").last?.lowercased() ?? ""
+	return appBundleId.components(separatedBy: ".").last?.lowercased() ?? ""
 }
-
 func getHTTPHandlers() -> [String: String] {
-    guard let url = URL(string: "http://") else { return [:] }
-    let apps = NSWorkspace.shared.urlsForApplications(toOpen: url)
-
-    var dict: [String: String] = [:]
-
-    for appUrl in apps {
-        let bundleId = Bundle(url: appUrl)?.bundleIdentifier ?? ""
-        dict[appName(fromBundleId: bundleId)] = bundleId
-    }
-
-    return dict
+	guard let url = URL(string: "http://") else { return [:] }
+	let apps = NSWorkspace.shared.urlsForApplications(toOpen: url)
+	var dict: [String: String] = [:]
+	for appUrl in apps {
+		let bundleId = Bundle(url: appUrl)?.bundleIdentifier ?? ""
+		dict[appName(fromBundleId: bundleId)] = bundleId
+	}
+	return dict
 }
-
 func getCurrentHTTPHandler() -> String {
-    guard let httpUrl = URL(string: "http://"),
-        let handler = NSWorkspace.shared.urlForApplication(toOpen: httpUrl)
-    else {
-        return ""
-    }
-
-    return Bundle(url: handler)?.bundleIdentifier ?? ""
+	guard let httpUrl = URL(string: "http://"),
+		let handler = NSWorkspace.shared.urlForApplication(toOpen: httpUrl)
+	else { return "" }
+	return Bundle(url: handler)?.bundleIdentifier ?? ""
 }
-
 func setDefaultHandler(urlScheme: String, handlerBundleId: String) {
-    LSSetDefaultHandlerForURLScheme(urlScheme as CFString, handlerBundleId as CFString)
+	LSSetDefaultHandlerForURLScheme(urlScheme as CFString, handlerBundleId as CFString)
 }
-
 let arguments = CommandLine.arguments
 let target = arguments.count == 1 ? "" : arguments[1].lowercased()
-
 let edgeCanaryBundleId = "com.microsoft.edgemac.Canary"
 let safariTechPreviewBundleId = "com.apple.SafariTechnologyPreview"
 let handlers = getHTTPHandlers()
 let currentHandlerBundleId = getCurrentHTTPHandler()
-
 if target.isEmpty || target == "toggle" {
-    if currentHandlerBundleId == edgeCanaryBundleId {
-        setDefaultHandler(urlScheme: "http", handlerBundleId: safariTechPreviewBundleId)
-        setDefaultHandler(urlScheme: "https", handlerBundleId: safariTechPreviewBundleId)
-        print("Switched from Edge Canary to Safari Technology Preview")
-    } else if currentHandlerBundleId == safariTechPreviewBundleId {
-        setDefaultHandler(urlScheme: "http", handlerBundleId: edgeCanaryBundleId)
-        setDefaultHandler(urlScheme: "https", handlerBundleId: edgeCanaryBundleId)
-        print("Switched from Safari Technology Preview to Edge Canary")
-    } else {
-        setDefaultHandler(urlScheme: "http", handlerBundleId: edgeCanaryBundleId)
-        setDefaultHandler(urlScheme: "https", handlerBundleId: edgeCanaryBundleId)
-        print("Set Edge Canary as default (was neither Edge Canary nor Safari Technology Preview)")
-    }
+	if currentHandlerBundleId == edgeCanaryBundleId {
+		setDefaultHandler(urlScheme: "http", handlerBundleId: safariTechPreviewBundleId)
+		setDefaultHandler(urlScheme: "https", handlerBundleId: safariTechPreviewBundleId)
+		print("Switched from Edge Canary to Safari Technology Preview")
+	} else if currentHandlerBundleId == safariTechPreviewBundleId {
+		setDefaultHandler(urlScheme: "http", handlerBundleId: edgeCanaryBundleId)
+		setDefaultHandler(urlScheme: "https", handlerBundleId: edgeCanaryBundleId)
+		print("Switched from Safari Technology Preview to Edge Canary")
+	} else {
+		setDefaultHandler(urlScheme: "http", handlerBundleId: edgeCanaryBundleId)
+		setDefaultHandler(urlScheme: "https", handlerBundleId: edgeCanaryBundleId)
+		print("Set Edge Canary as default (was neither Edge Canary nor Safari Technology Preview)")
+	}
 } else {
-    let currentHandlerName = appName(fromBundleId: currentHandlerBundleId)
-    
-    if target == currentHandlerName {
-        print("\(target) is already set as the default HTTP handler")
-    } else if let targetHandler = handlers[target] {
-        setDefaultHandler(urlScheme: "http", handlerBundleId: targetHandler)
-        setDefaultHandler(urlScheme: "https", handlerBundleId: targetHandler)
-        print("Set \(target) as the default HTTP handler")
-    } else {
-        print("Browser '\(target)' not found")
-    }
+	let currentHandlerName = appName(fromBundleId: currentHandlerBundleId)
+	if target == currentHandlerName {
+		print("\(target) is already set as the default HTTP handler")
+	} else if let targetHandler = handlers[target] {
+		setDefaultHandler(urlScheme: "http", handlerBundleId: targetHandler)
+		setDefaultHandler(urlScheme: "https", handlerBundleId: targetHandler)
+		print("Set \(target) as the default HTTP handler")
+	} else {
+		print("Browser '\(target)' not found")
+	}
 }
